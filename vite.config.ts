4158e74afb57de35d6d23859acf55a72145c96da
vineethayasa <vineethareddy185@gmail.com>
2023-11-08 12:32:52 +0530

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Sports Center application",
+        short_name: "Sports Center",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "/favicon-16x16.png",
+            type: "image/png",
+            sizes: "16x16",
+          },
+          {
+            src: "/favicon-32x32.png",
+            type: "image/png",
+            sizes: "32x32",
+          },
+          {
+            src: "/pwa-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/pwa-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#AAF",
+      },
+    }),
+  ],
 });
